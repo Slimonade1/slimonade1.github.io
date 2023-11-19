ab6eea29932cec48904069e4c6b1ee2bab6221f1
Slimonade1 <Simon.123toftemark@gmail.com>
2023-11-19 22:09:14 +0100

    Florida mayhem

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 // No clue hvorfor arrayLike ikke er et array
 const arrayLike = document.getElementsByTagName('section')
 const sections = Array.from(arrayLike)
 
 sections.forEach((section, index) =>{
-    section.style.backgroundColor = `rgba(${102 + index*50}, 142, 220, 0.45)`
+    section.style.backgroundColor = `rgba(${102 + index * 50}, 142, 220, 0.45)`
+    section.style.borderColor = `rgb(${41 + index * 30}, 20, 176)`
 })
