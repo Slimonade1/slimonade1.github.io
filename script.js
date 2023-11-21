39d29cbfd926d3e15e82d3bb960e8ab6f7aee4a0
Slimonade1 <Simon.123toftemark@gmail.com>
2023-11-21 15:20:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const arrayLike = document.getElementsByTagName('section')
 const sections = Array.from(arrayLike)
 
 sections.forEach((section, index) =>{
-    section.style.backgroundColor = `rgba(${102 + index * 30}, 142, 220, 0.45)`
+    section.style.backgroundColor = `rgba(${52 + index * 40}, 142, 220, 0.45)`
     section.style.borderColor = `rgb(${41 + index * 20}, 20, 176)`
 })
